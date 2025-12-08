85ddb9df30405ece2d5cbb2074917626d8c58fa1
Степан <stepancat61@gmail.com>
2025-12-08 06:58:53 +0300

    small rfct plnt blc

--- frontend/src/pages/main/Main.jsx
+++ frontend/src/pages/main/Main.jsx
@@ -1,21 +1,25 @@
 import Clock from "../../ArtComponents/Clock";
 import Planet from "../../ArtComponents/Planet";
-import { UserContext } from "../../context/UserContext";
 import styles from "./Main.module.css";
 import { useContext, useEffect, useState } from "react";
-import Loader from "../../ArtComponents/Loader";
 import { useAuth } from "../../context/AuthContext";
+
+const PlanetBlock = () => {
+  return (
+    <div className={styles.wiggleWrapper}>
+      <div className={styles.wiggle}>
+        <Planet />
+      </div>
+    </div>
+  );
+};
 
 export default function Main() {
   const { userData } = useAuth();
 
   return (
     <div className={styles.pageCont}>
-      <div className={styles.wiggleWrapper}>
-        <div className={styles.wiggle}>
-          <Planet />
-        </div>
-      </div>
+      <PlanetBlock />
       <div className={styles.mainPage}>
         <div className="pageContent">
           <div className={styles.clockSection}>
